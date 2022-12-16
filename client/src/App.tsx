942bc7b396388a0d638b54ebb1c160f6fee9cff2
yelinz515 <ggg06141@gmail.com>
2022-12-16 11:10:31 +0900

    🎨 초기 셋팅 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
+import Main from "./pages/Main";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Router>
         <Routes>
-          <Route path="/" element={<MainPage />} />
+          <Route path="/" element={<Main />} />
         </Routes>
       </Router>
-    </div>
+    </>
   );
 }
 
